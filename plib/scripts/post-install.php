37ddb901cd6110c0bb65e954463da985c20993e1
Eugene Kazakov <ekazakov@parallels.com>
2015-09-15 18:02:08 +0600

    Error in panel.log when upgrade extension Plesk News (fixes #1)

--- plib/scripts/post-install.php
+++ plib/scripts/post-install.php
@@ -4,6 +4,14 @@ pm_Context::init('panel-news');
 
 Modules_PanelNews_News::load();
 
+$tasks = pm_Scheduler::getInstance()->listTasks();
+foreach ($tasks as $task) {
+    if ('periodic-task.php' == $task->getCmd()) {
+        pm_Settings::set('periodic_task_id', $task->getId());
+        return;
+    }
+}
+
 $task = new pm_Scheduler_Task();
 $task->setSchedule(pm_Scheduler::$EVERY_DAY);
 $task->setCmd('periodic-task.php');
--- plib/scripts/pre-uninstall.php
+++ plib/scripts/pre-uninstall.php
@@ -3,5 +3,14 @@
 pm_Context::init('panel-news');
 
 $taskId = pm_Settings::get('periodic_task_id');
-$task = pm_Scheduler::getInstance()->getTaskById($taskId);
-pm_Scheduler::getInstance()->removeTask($task);
+if (!$taskId) {
+    return;
+}
+
+try {
+    $task = pm_Scheduler::getInstance()->getTaskById($taskId);
+    pm_Scheduler::getInstance()->removeTask($task);
+} catch (pm_Exception $e) {
+    echo $e->getMessage();
+}
+
