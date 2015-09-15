28de26803ec73b8b3615d285287d23432e0d86af
Eugene Kazakov <ekazakov@parallels.com>
2015-09-15 17:50:15 +0600

    Handle errors with invalid rss feed.

--- plib/library/News.php
+++ plib/library/News.php
@@ -5,13 +5,20 @@ class Modules_PanelNews_News
 
     public static function load()
     {
-        $url = 'http://www.parallels.com/products/plesk/rss';
-        $xml = simplexml_load_string(file_get_contents($url));
+        $url = 'http://www.odin.com/products/plesk/rss/';
+        $content = @file_get_contents($url);
+        if (false === $content) {
+            return;
+        }
+        $xml = simplexml_load_string($content);
+        if (false === $xml || !$xml->channel) {
+            return;
+        }
 
         $lastItem = $xml->channel->item;
 
-        pm_Settings::set('news_text', (string) $lastItem->title);
-        pm_Settings::set('news_link', (string) $lastItem->link);
+        pm_Settings::set('news_text', $lastItem ? (string) $lastItem->title : '');
+        pm_Settings::set('news_link', $lastItem ? (string) $lastItem->link : '');
     }
 
 }
